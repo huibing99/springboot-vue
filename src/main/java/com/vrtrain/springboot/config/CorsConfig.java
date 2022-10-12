ec1432eed740caea30e2bc72b18619ab21892e57
Huibing <hb62@qq.com>
2022-10-12 15:28:51 +0800

    更新环境配置

--- src/main/java/com/vrtrain/springboot/config/CorsConfig.java
+++ src/main/java/com/vrtrain/springboot/config/CorsConfig.java
@@ -18,10 +18,9 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-//        corsConfiguration.addAllowedOrigin(Arrays.asList("http://vr-scene.metastar-health.com/",
-//                "http://vr-web.metastar-health.com/"));
         corsConfiguration.addAllowedOrigin("http://vr-scene.metastar-health.com"); // 1 设置访问源地址
         corsConfiguration.addAllowedOrigin("http://vr-web.metastar-health.com"); // 1 设置访问源地址
+        corsConfiguration.addAllowedOrigin("http://localhost:8080/"); // 1 设置访问源地址
         corsConfiguration.addAllowedHeader("*"); // 2 设置访问源请求头
         corsConfiguration.addAllowedMethod("*"); // 3 设置访问源请求方法
         corsConfiguration.setAllowCredentials(true);
